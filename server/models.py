a54a8a66a8e34582db876a88099f33098a4b17e7
Arthur Ish <arthurishdev@gmail.com>
2025-05-14 00:51:40 -0700

    user model added

--- server/models.py
+++ server/models.py
@@ -4,3 +4,21 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+# User model with id, username, and password, and excercise associations
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    exercises = db.relationship('Exercise', backref='user', cascade='all, delete-orphan')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+#category model with id, name, and exercise associations
+
+# exercise model with id, name, description, and category_id, and user_exercise associations
